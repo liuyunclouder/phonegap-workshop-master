7e72b507f8396680fae389336c5daeb781180e23
lingwu.tyf@taobao.com <lingwu.tyf@taobao.com>
2013-03-12 10:53:55 +0800

    up

--- js/main.js
+++ js/main.js
@@ -22,6 +22,7 @@ var app = {
     },
 
     initialize: function() {
+        var self = this;
         this.store = new MemoryStore(function() {
             self.showAlert('Store Initialized', 'Info');
         });
